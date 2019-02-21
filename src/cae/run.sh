d3fd237d2c43971b786a57af5dd587fe783bdd4e
Yi Fung <yfung@gypsum.cm.cluster>
2019-02-21 12:06:51 -0500

    testing my branch out

--- src/cae/run.sh
+++ src/cae/run.sh
@@ -4,11 +4,12 @@
 #SBATCH -e outputs/errors/err_%j.err    # File to which STDERR will be written
 #SBATCH --output=outputs/logs/%j.txt    # Output file
 #SBATCH --partition=1080ti-long
-#SBATCH --ntasks=8                     # Set to max_workers + 2
+#SBATCH --ntasks=2                     # Set to max_workers + 2
 #SBATCH --time=02-00:00                 # Runtime in D-HH:MM
-#SBATCH --mem=45000
+#SBATCH --mem=24000
 #SBATCH --gres=gpu:1
 
-python main.py
+#python main.py
+python classifier.py
 sleep 1
 exit
